8a6b18516ff735c62628ee1b2d9f6405bfc2cee9
Akshat Mishra <akshatmishra317@gmail.com>
2023-06-28 17:07:00 +0530

    main

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -21,7 +21,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
 
-        const response = await fetch('http://localhost:8080/api/v1/pic', {
+        const response = await fetch('https://pic-trigger.onrender.com/api/v1/pic', {
           method:'POST',
           headers: {
             'Content-type': 'application/json',
@@ -48,7 +48,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://pic-trigger.onrender.com/api/v1/post', {
           method:'POST',
           headers: {
             'Content-Type': 'application/json'
